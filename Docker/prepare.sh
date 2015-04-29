c3ee6e4460779a82546dd2b49c99181723071d6d
Michael Vorburger <mvorburger@temenos.com>
2015-04-29 17:59:37 +0200

    WIP

---
+++ Docker/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+set -x
+
+apt-get dist-upgrade -y --no-install-recommends
---
+++ deploy.sh
@@ -0,0 +1,28 @@
+# Exit the script if any command returns a non-true return value (http://www.davidpashley.com/articles/writing-robust-shell-scripts/)
+set -e
+# Print commands and their arguments as they are executed, so you see what's happening.
+set -x
+
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+
+# Must use absolute instead of relative path here
+MB_HOME=/home/mvorburger/PreSalesDS/base_mb_t24brpdev_7
+export TAFJ_HOME=$MB_HOME/TAFJ
+export PATH=$TAFJ_HOME/bin:$PATH
+
+export T24_HOME=$MB_HOME/h2/UD
+export PACKAGE=$T24_HOME/package
+export DS_PACKAGE_UPDATES=$T24_HOME/DSPackageUpdates
+rm -rf $PACKAGE; mkdir $PACKAGE
+rm -rf $DS_PACKAGE_UPDATES; mkdir $DS_PACKAGE_UPDATES
+
+if [[ $# -ne 1 ]]; then
+	echo "Missing argument"
+	exit 1
+fi
+echo $1 copying to $PACKAGE
+cp $1 $PACKAGE
+
+tRun -cf tafj T24PackageInstaller
+# TODO Need to run this several times?? How many? (Required for some use cases.. find details, and test)
+tRun -cf tafj packageDataInstaller
