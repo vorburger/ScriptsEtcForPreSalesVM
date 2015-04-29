c8ddbabffbb8dea0c20315c0caa4f34e4d33de7f
Michael Vorburger <mvorburger@temenos.com>
2015-04-29 19:54:16 +0200

    WIP

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -6,8 +6,15 @@ export LC_ALL=C
 export DEBIAN_FRONTEND=noninteractive
 minimal_apt_get_install='apt-get install -y --no-install-recommends'
 
-apt-get dist-upgrade -y --no-install-recommends
-apt-get install -y --no-install-recommends nano psmisc
+# https://docs.docker.com/articles/dockerfile_best-practices/ :
+# Avoid RUN apt-get upgrade or dist-upgrade
+# apt-get dist-upgrade -y --no-install-recommends
+# Don’t do RUN apt-get update on a single line  (but along with apt-get install)
+apt-get update && $minimal_apt_get_install \
+	nano \
+	psmisc \
+	unzip \
+	openjdk-7-jdk
 
 ## Fix locale.
 # $minimal_apt_get_install language-pack-en
@@ -16,12 +23,13 @@ apt-get install -y --no-install-recommends nano psmisc
 # echo -n en_US.UTF-8 > /etc/container_environment/LANG
 # echo -n en_US.UTF-8 > /etc/container_environment/LC_CTYPE
 
+# Clean up APT when done.
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
 # Run Hudson once during image creation, so that it's faster first time
 # RUN java -jar hudson.war
 
-# Clean up APT when done.
-apt-get clean
-# rm -rf /var/lib/apt/lists/*
-
 rm -rf /build
 rm -rf /tmp/* /var/tmp/*
+
