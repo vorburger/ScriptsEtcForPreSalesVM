ef71b2b2a7c9855328c8f405a7b5501b146eb56a
Michael Vorburger <mvorburger@temenos.com>
2015-05-06 20:34:35 +0200

    fix_utp.sh WIP

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ wget -cN http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 # NOTE: We *MUST* use EXACTLY v3.0.4 here, otherwise mvn -o Offline doesn't work
 wget -cN http://archive.apache.org/dist/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
 wget -cN -r -l1 "http://p2.oams.com/dist/latest/master/" -A "t24-binaries*.zip" -O t24-binaries_LATEST.zip
-# TODO where to internally download sth. like base_mb_t24brpdev_7.tar.gz from??
+# TODO wget http://cloud-ivy.temenosgroup.com/build/utp/UTP_Build_Latest.7z
+# unzip UTP_Build_Latest.7z
+# ./fix_utp.sh UTP_Build_Latest
 cd -
 
 docker build -t dsdemo .
---
+++ fix_utp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+chmod +x $1/jboss/bin/*.sh
+# rm $1/TAFJ/bin/*.bat
+chmod +x $1/TAFJ/bin/*
+dos2unix $1/TAFJ/bin/*
+
