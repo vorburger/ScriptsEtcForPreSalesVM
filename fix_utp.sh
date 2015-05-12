62bcda98ceb9b5ebdeecc6a52bf395a17e960d65
Michael Vorburger <mvorburger@temenos.com>
2015-05-12 15:12:19 +0200

    Need to use : instead of ; separate in temn.tafj.directory.precompile

--- fix_utp.sh
+++ fix_utp.sh
@@ -14,5 +14,8 @@ dos2unix $1/TAFJ/bin/*
 # https://github.com/temenostech/Hothouse/issues/987 is *STILL* NOK :-(
 sed -i 's/\\/\//g' $1/TAFJ/conf/tafj.properties
 
+# Another Windows/Linux cross platform issue.. it's wrong to catch up one-by-one; the package used should use the TAFJ Maven plug-in to create the TAFJ home instead :(
+sed -i 's/Primary.jar;/Primary.jar:/g' $1/TAFJ/conf/tafj.properties
+
 # https://github.com/temenostech/Hothouse/issues/162 is *STILL* NOK :-(
 mv $1/jboss/modules/com/temenos/t24/main/Module.xml $1/jboss/modules/com/temenos/t24/main/module.xml
